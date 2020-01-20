074658cb3a7712723def866828d6af890489a3f0
Tristan Sloughter <t@crashfast.com>
2020-01-20 08:11:40 -0700

    use remote build and add additional architectures

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-OTP_VERSION=${1:-22.2.2}
+OTP_VERSION=${1:-22.2.3}
 REBAR3_VERSION=${2:-3.13.0}
 
 OTP_VERSION=$OTP_VERSION REBAR3_VERSION=$REBAR3_VERSION envsubst < snapcraft.yaml.tmpl > snap/snapcraft.yaml
 
 echo "Building OTP-${OTP_VERSION} with rebar3 ${REBAR3_VERSION}"
 
-snapcraft
+snapcraft remote-build
