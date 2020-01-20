7ef246a26de753346e91fa311afb9147e271474d
Tristan Sloughter <t@crashfast.com>
2020-01-20 15:55:11 -0700

    fix envsubst command to not replace  in template

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -e
 OTP_VERSION=${1:-22.2.3}
 REBAR3_VERSION=${2:-3.13.0}
 
-OTP_VERSION=$OTP_VERSION REBAR3_VERSION=$REBAR3_VERSION envsubst < snapcraft.yaml.tmpl > snap/snapcraft.yaml
+OTP_VERSION=$OTP_VERSION REBAR3_VERSION=$REBAR3_VERSION \
+           envsubst '${OTP_VERSION} ${REBAR3_VERSION}' < snapcraft.yaml.tmpl > snap/snapcraft.yaml
 
 echo "Building OTP-${OTP_VERSION} with rebar3 ${REBAR3_VERSION}"
 
