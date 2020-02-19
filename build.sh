88121b875604cf7db98a935926dd5c3e758447e3
Tristan Sloughter <t@crashfast.com>
2020-02-18 20:58:27 -0700

    use path of script run to find erts dir

--- build.sh
+++ build.sh
@@ -2,11 +2,25 @@
 
 set -e
 
-OTP_VERSION=${1:-22.2.6}
-REBAR3_VERSION=${2:-3.13.0}
+OTP_VERSION=${1:-22.2.7}
+ERTS_VERSION=${2:-10.6.4}
+REBAR3_VERSION=${3:-3.13.0}
 
-OTP_VERSION=$OTP_VERSION REBAR3_VERSION=$REBAR3_VERSION \
-           envsubst '${OTP_VERSION} ${REBAR3_VERSION}' < snapcraft.yaml.tmpl > snap/snapcraft.yaml
+OTP_VERSION=$OTP_VERSION REBAR3_VERSION=$REBAR3_VERSION ERTS_VERSION=$ERTS_VERSION \
+           envsubst '${OTP_VERSION} ${ERTS_VERSION} ${REBAR3_VERSION}' < snapcraft.yaml.tmpl > snap/snapcraft.yaml
+
+ERTS_VERSION=$ERTS_VERSION \
+           envsubst '${ERTS_VERSION}' < erl.tmpl > snap/local/erl1
+
+ERTS_VERSION=$ERTS_VERSION \
+           envsubst '${ERTS_VERSION}' < erl1.tmpl > snap/local/erl2
+
+ERTS_VERSION=$ERTS_VERSION \
+           envsubst '${ERTS_VERSION}' < erl2.tmpl > snap/local/erl3
+
+chmod +x snap/local/erl1
+chmod +x snap/local/erl2
+chmod +x snap/local/erl3
 
 echo "Building OTP-${OTP_VERSION} with rebar3 ${REBAR3_VERSION}"
 
